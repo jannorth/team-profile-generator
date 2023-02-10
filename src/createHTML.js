e014d2d91d5fc0d6b11ec9d65c3180183516a1cf
jannorth <116915245+jannorth@users.noreply.github.com>
2023-02-10 10:36:32 -0500

    added code to generate html formatting:

--- src/createHTML.js
+++ src/createHTML.js
@@ -3,15 +3,72 @@ function createHTML(teamArray) {
 
     function createManager(manager) {
         return `
-        <h2>${manager.getRole()}</h2>
-        <h2>${manager.getName()}</h2>
-        <h2>${manager.getId()}</h2>
-        <h2>${manager.getEmail()}</h2>
-        <h2>${manager.getOfficeNumber()}</h2>
+        <div class="emp-info">
+            <div class="info-heading">
+                <h2>${manager.getName()}</h2>
+                <h2>${manager.getRole()}</h2>
+                <div class="emp-contact">
+                <ul>
+                    <li>
+                        <h3>${manager.getId()}</h3>
+                    </li>
+                    <li>
+                        <h3>${manager.getEmail()}</h3>
+                    </li>
+                    <li>
+                        <h3>${manager.getOfficeNumber()}</h3>
+                    </li>
+                </ul>
+            </div>
+        </div>
         `
     }
-
-
+    function createEngineer(engineer) {
+        return `
+        <div class="emp-info">
+            <div class="info-heading">
+                <h2>${engineer.getName()}</h2>
+                <h2>${engineer.getRole()}</h2>
+                <div class="emp-contact">
+                    <ul>
+                        <li>
+                            <h3>${engineer.getId()}</h3>
+                        </li>
+                        <li>
+                            <h3>${engineer.getEmail()}</h3>
+                        </li>
+                        <li>
+                            <h3>${engineer.getGithub()}</h3>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    function createIntern(intern) {
+        return `
+        </div class="emp-info">
+            <div class="info-heading">
+                <h2>${intern.getName()}</h2>
+                <h2>${intern.getRole()}</h2>
+                <div class="emp-contact">
+                    <ul>
+                        <li>
+                            <h3>${intern.getId()}</h3>
+                        </li>
+                        <li>
+                            <h3>${intern.getEmail()}</h3>
+                        </li>
+                        <li>
+                            <h3>${intern.getSchool()}</h3>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        `
+    }
 
 for (var i = 0; i < teamArray.length; i ++) {
     if (teamArray[i].getRole()==="Manager"){
@@ -44,9 +101,6 @@ module.exports = teamArray => {
         ${createHTML(teamArray)}
     </div>
 </body>
-</html>
-    
-    
-    
+</html>    
     `
 }
